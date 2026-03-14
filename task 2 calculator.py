a6efdabfe4a6681c6718727e35163cfc834d90da
vaishnavikawade43-ai <vaishnavikawade43@gmail.com>
2026-03-14 12:34:42 +0530

    Add files via upload

---
+++ task 2 calculator.py
@@ -0,0 +1,37 @@
+# Simple Calculator Program
+
+print("Simple Calculator")
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+# Take input from user
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform calculation
+if choice == '1':
+    result = num1 + num2
+    print("Result:", result)
+
+elif choice == '2':
+    result = num1 - num2
+    print("Result:", result)
+
+elif choice == '3':
+    result = num1 * num2
+    print("Result:", result)
+
+elif choice == '4':
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error! Division by zero.")
+
+else:
+    print("Invalid input")
